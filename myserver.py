eee991612e8936279959d234d3598ca35fc8def6
Fudesign55 <fudesign55@gmail.com>
2026-01-20 13:35:34 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -1,18 +1,16 @@
 from flask import Flask
 from threading import Thread
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AURACITY BOT is running!"
+    return "Server is running!"
 
-def _run():
-    port = int(os.getenv("PORT", "8080"))
-    app.run(host="0.0.0.0", port=port)
+def run():
+    app.run(host="0.0.0.0", port=8080)
 
 def server_on():
-    t = Thread(target=_run)
+    t = Thread(target=run)
     t.daemon = True
     t.start()
